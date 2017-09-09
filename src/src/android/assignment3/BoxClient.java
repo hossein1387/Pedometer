6db262ab28a9e623e17506741f929bea38d0d271
Hossein <m.h.askari.hemmat@gmail.com>
2017-09-09 17:48:42 -0400

    remove personal BOX client ids

--- src/src/android/assignment3/BoxClient.java
+++ src/src/android/assignment3/BoxClient.java
@@ -5,8 +5,8 @@ import com.box.boxandroidlibv2.BoxAndroidClient;
 
 public class BoxClient extends Application {
 
-    public static final String CLIENT_ID = "fe82ew6hlhw82nh271j6cs5j7yoypabc";
-    public static final String CLIENT_SECRET = "lbrv0Ct5wpWYhXig1QXahshiKRsa4Ldg";
+    public static final String CLIENT_ID = "CLIENT_ID";
+    public static final String CLIENT_SECRET = "CLIENT_SECRET";
     public static final String REDIRECT_URL = "http://localhost";
     
     private BoxAndroidClient mClient;
